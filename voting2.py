e2a360f2d6c8ef1324db6d35f176abcd2b0a107e
ABHISHEK UPADHYAY <abhisheku722@protonmail.com>
2020-07-24 16:26:41 +0530

    Update voting2.py

--- voting2.py
+++ voting2.py
@@ -2,19 +2,18 @@ import tkinter as tk
 from tkinter import simpledialog
 from tkinter import messagebox
 
-application_window=tk.Tk()
+application_window = tk.Tk()
 
 answer= simpledialog.askinteger("Voting Application", "Enter the number of candidates:", minvalue=1, maxvalue=30)
-
 if answer is not None:
-    messagebox.askokcancel("Voting Application", "The number of candidates:"+answer)
+    messagebox.askokcancel("Voting Application", "The number of candidates:", answer)
 else:
     messagebox.askokcancel("Voting Application", "There are no candidates? ")
 
 answer=simpledialog.askstring("Voting Application", "What is the name of Candidate?")
 
 if answer is not None:
-    messagebox.askokcancel("Voting Application", "Candidate name is"+answer)
+    messagebox.askokcancel("Voting Application", "Candidate name is", answer)
 else:
     messagebox.askokcancel("Voting Application", "Candidate has no name?")
 
@@ -25,6 +24,6 @@ if answer is not None:
 else:
     messagebox.askokcancel("Voting Application", "No age available")
 
-answer=simpledialog.askokcancel("Voting Application", "Who do you want to vote for? Enter Name")
+answer=simpledialog.askstring("Voting Application", "Who do you want to vote for? Enter Name")
 
 messagebox.askokcancel("Voting Application", "And the Vote is casted for ", answer)
